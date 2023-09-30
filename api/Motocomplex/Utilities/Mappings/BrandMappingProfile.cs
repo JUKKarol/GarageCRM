3910d477483f6d764a818041b010e196bdd673ae
JUKKarol <karolprzybylak2@gmail.com>
2023-09-30 10:20:39 +0200

    Add mappings for model and brand

---
+++ api/Motocomplex/Utilities/Mappings/BrandMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Motocomplex.DTOs.BrandDTOs;
+using Motocomplex.DTOs.ModelDTOs;
+using Motocomplex.Entities;
+
+namespace Motocomplex.Utilities.Mappings
+{
+    public class BrandMappingProfile : Profile
+    {
+        public BrandMappingProfile()
+        {
+            CreateMap<BrandDisplayDto, Brand>().ReverseMap();
+            CreateMap<BrandCreateDto, Brand>().ReverseMap();
+            CreateMap<BrandUpdateDto, Brand>().ReverseMap();
+        }
+    }
+}
---
+++ api/Motocomplex/Utilities/Mappings/ModelMappingProfile.cs.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Motocomplex.DTOs.ModelDTOs;
+using Motocomplex.Entities;
+
+namespace Motocomplex.Utilities.Mappings
+{
+    public class ModelMappingProfile : Profile
+    {
+        public ModelMappingProfile()
+        {
+            CreateMap<ModelDisplayDto, Model>().ReverseMap();
+            CreateMap<ModelCreateDto, Model>().ReverseMap();
+            CreateMap<ModelUpdateDto, Model>().ReverseMap();
+        }
+    }
+}
