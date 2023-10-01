c3b877a3162ed3499573da44aa3af7e3a523cc89
JUKKarol <karolprzybylak2@gmail.com>
2023-10-01 15:13:59 +0200

    Add car service

---
+++ api/Motocomplex/Services/CarService/CarService.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using Motocomplex.Data.Repositories.BrandRepository;
+using Motocomplex.Data.Repositories.CarRepository;
+using Motocomplex.DTOs.CarDTOs;
+using Motocomplex.DTOs.SharedDTOs;
+using Motocomplex.Entities;
+using Sieve.Models;
+
+namespace Motocomplex.Services.CarService
+{
+    public class CarService : ICarService
+    {
+        private readonly ICarRepository _carRepository;
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+
+        public CarService(ICarRepository carRepository, IBrandRepository brandRepository, IMapper mapper)
+        {
+            _carRepository = carRepository;
+            _brandRepository = brandRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<CarDisplayDto> GetCarById(Guid carId)
+        {
+            var car = await _carRepository.GetCarById(carId);
+            return _mapper.Map<CarDisplayDto>(car);
+        }
+
+        public async Task<CarDisplayDto> GetCarByVin(string vin)
+        {
+            var car = await _carRepository.GetCarByVin(vin);
+            return _mapper.Map<CarDisplayDto>(car);
+        }
+
+        public async Task<RespondListDto<CarDisplayDto>> GetCars(SieveModel query)
+        {
+            int pageSize = query.PageSize != null ? (int)query.PageSize : 40;
+
+            var cars = await _carRepository.GetCars(query);
+            var carsDto = _mapper.Map<List<CarDisplayDto>>(cars);
+
+            RespondListDto<CarDisplayDto> respondListDto = new RespondListDto<CarDisplayDto>();
+            respondListDto.Items = carsDto;
+            respondListDto.ItemsCount = await _carRepository.GetCarsCount(query);
+            respondListDto.PagesCount = (int)Math.Ceiling((double)respondListDto.ItemsCount / pageSize);
+
+            return respondListDto;
+        }
+
+        public async Task<CarDisplayDto> CreateCar(CarCreateDto carDto)
+        {
+            var car = _mapper.Map<Car>(carDto);
+            await _carRepository.CreateCar(car);
+
+            return _mapper.Map<CarDisplayDto>(car);
+        }
+
+        public async Task<CarDisplayDto> UpdateCar(CarUpdateDto carDto)
+        {
+            var car = _mapper.Map<Car>(carDto);
+            await _carRepository.UpdateCar(car);
+
+            return _mapper.Map<CarDisplayDto>(car);
+        }
+    }
+}
---
+++ api/Motocomplex/Services/CarService/ICarService.cs
@@ -0,0 +1,16 @@
+﻿using Motocomplex.DTOs.CarDTOs;
+using Motocomplex.DTOs.SharedDTOs;
+using Sieve.Models;
+using System.Threading.Tasks;
+
+namespace Motocomplex.Services.CarService
+{
+    public interface ICarService
+    {
+        Task<CarDisplayDto> GetCarById(Guid carId);
+        Task<CarDisplayDto> GetCarByVin(string vin);
+        Task<RespondListDto<CarDisplayDto>> GetCars(SieveModel query);
+        Task<CarDisplayDto> CreateCar(CarCreateDto carDto);
+        Task<CarDisplayDto> UpdateCar(CarUpdateDto carDto);
+    }
+}
