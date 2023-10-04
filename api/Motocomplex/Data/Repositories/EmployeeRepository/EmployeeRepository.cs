17c88ac81a4cc6de3055eb1ac6f96c0231f6ad5b
JUKKarol <karolprzybylak2@gmail.com>
2023-10-04 08:55:20 +0200

    Add employee repository

---
+++ api/Motocomplex/Data/Repositories/EmployeeRepository/EmployeeRepository.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Motocomplex.Entities;
+using Sieve.Models;
+using Sieve.Services;
+
+namespace Motocomplex.Data.Repositories.EmployeeRepository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly MotocomplexContext _db;
+        private readonly ISieveProcessor _sieveProcessor;
+
+        public EmployeeRepository(MotocomplexContext db, ISieveProcessor sieveProcessor)
+        {
+            _db = db;
+            _sieveProcessor = sieveProcessor;
+        }
+
+        public async Task<Employee> GetEmployeeById(Guid employeeId)
+        {
+            return await _db.Employee.FirstOrDefaultAsync(c => c.Id == employeeId);
+        }
+
+        public async Task<List<Employee>> GetEmployees(SieveModel query)
+        {
+            var employees = _db
+                .Employee
+                .AsNoTracking()
+                .AsQueryable();
+
+            return await _sieveProcessor
+                .Apply(query, employees)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<int> GetEmployeesCount(SieveModel query)
+        {
+            var employees = _db
+                .Employee
+                .AsNoTracking()
+                .AsQueryable();
+
+            return await _sieveProcessor
+                .Apply(query, employees, applyPagination: false)
+                .CountAsync();
+        }
+
+        public async Task<Employee> CreateEmployee(Employee employee)
+        {
+            await _db.Employee.AddAsync(employee);
+            await _db.SaveChangesAsync();
+
+            return employee;
+        }
+
+        public async Task<Employee> UpdateEmployee(Employee updatedEmployee)
+        {
+            var employee = await _db.Employee.FirstOrDefaultAsync(c => c.Id == updatedEmployee.Id);
+
+            var employeeCraetedAt = employee.CreatedAt;
+            updatedEmployee.CreatedAt = employeeCraetedAt;
+
+            var entry = _db.Entry(employee);
+            entry.CurrentValues.SetValues(updatedEmployee);
+
+            await _db.SaveChangesAsync();
+
+            return employee;
+        }
+
+        public async Task<Employee> AddToArchive(Guid employeeId)
+        {
+            var employee = await _db.Employee.FirstOrDefaultAsync(c => c.Id == employeeId);
+            employee.IsArchive = true;
+            await _db.SaveChangesAsync();
+
+            return employee;
+        }
+
+        public async Task<Employee> BackFromArchive(Guid employeeId)
+        {
+            var employee = await _db.Employee.FirstOrDefaultAsync(c => c.Id == employeeId);
+            employee.IsArchive = false;
+            await _db.SaveChangesAsync();
+
+            return employee;
+        }
+    }
+}
---
+++ api/Motocomplex/Data/Repositories/EmployeeRepository/IEmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using Motocomplex.Entities;
+using Sieve.Models;
+
+namespace Motocomplex.Data.Repositories.EmployeeRepository
+{
+    public interface IEmployeeRepository
+    {
+        Task<Employee> GetEmployeeById(Guid employeeId);
+        Task<List<Employee>> GetEmployees(SieveModel query);
+        Task<int> GetEmployeesCount(SieveModel query);
+        Task<Employee> CreateEmployee(Employee employee);
+        Task<Employee> UpdateEmployee(Employee updatedEmployee);
+        Task<Employee> AddToArchive(Guid employeeId);
+        Task<Employee> BackFromArchive(Guid employeeId);
+        }
+}
