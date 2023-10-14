90b83aea181a238c231d6b9f991705a98630485a
JUKKarol <karolprzybylak2@gmail.com>
2023-10-14 22:36:45 +0200

    Add dto for mass model insert

---
+++ api/Motocomplex/DTOs/BrandModelDTOs/BrandModelCreateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Motocomplex.DTOs.BrandModelDTOs
+{
+    public class BrandModelCreateDto
+    {
+        public string BrandName { get; set; }
+        public List<string> ModelNames { get; set; }
+    }
+}
