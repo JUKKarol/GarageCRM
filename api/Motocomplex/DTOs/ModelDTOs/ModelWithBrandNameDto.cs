7d4b74ce2a9efd3e68294bb1692ea66160deb0b2
JUKKarol <karolprzybylak2@gmail.com>
2023-11-07 18:41:43 +0100

    Add dto for view

---
+++ api/Motocomplex/DTOs/ModelDTOs/ModelWithBrandNameDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Motocomplex.DTOs.ModelDTOs
+{
+    public class ModelWithBrandNameDto
+    {
+        public Guid ModelId { get; set; }
+        public DateTime ModelCreatedAt { get; set; }
+        public DateTime ModelUpdatedAt { get; set; }
+        public Guid ModelBrandId { get; set; }
+        public string ModelName { get; set; }
+        public string BrandName { get; set; }
+    }
+}
